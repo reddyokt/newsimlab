0b9741300a917f4078b6ad4b048aa9b1ed8a33fc
reddyokt <reddyoktariawan@gmail.com>
2024-01-11 08:49:18 +0700

    update role migration

--- database/migrations/2024_01_04_114816_create_roles_table.php
+++ database/migrations/2024_01_04_114816_create_roles_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -15,10 +17,20 @@ return new class extends Migration
             $table->id();
             $table->string('role_name');
             $table->string('CODE');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->string('created_by');
             $table->timestamps();
         });
+        Role::create(['role_name' => 'Superadmin','CODE' => 'SUP', 'created_by' => '1']);
+        Role::create(['role_name' => 'Ketua PWA','CODE' => 'PWA1', 'created_by' => '1']);
+        Role::create(['role_name' => 'Ketua PDA','CODE' => 'PDA1', 'created_by' => '1']);
+        Role::create(['role_name' => 'Ketua Majelis dan Lembaga Wilayah','CODE' => 'MWA1', 'created_by' => '1']);
+        Role::create(['role_name' => 'Ketua Majelis dan Lembaga Daerah','CODE' => 'MDA1', 'created_by' => '1']);
+        Role::create(['role_name' => 'Sekretaris PWA','CODE' => 'PWA2', 'created_by' => '1']);
+        Role::create(['role_name' => 'Sekretaris PDA','CODE' => 'PDA2', 'created_by' => '1']);
+        Role::create(['role_name' => 'Sekretaris Majelis dan Lembaga Wilayah','CODE' => 'MWA2', 'created_by' => '1']);
+        Role::create(['role_name' => 'Sekretaris Majelis dan Lembaga Daerah','CODE' => 'MDA2', 'created_by' => '1']);
+
     }
 
     /**
