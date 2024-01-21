041664b037cf41d1216efd56a5c3592c454614b3
reddyokt <reddyoktariawan@gmail.com>
2024-01-21 21:51:32 +0700

    menumodels

--- database/migrations/2024_01_06_060252_create_menu_table.php
+++ database/migrations/2024_01_06_060252_create_menu_table.php
@@ -22,21 +22,21 @@ return new class extends Migration
             $table->date('deleted_at')->nullable();
             $table->timestamps();
         });
-        Menu::create(['name' => 'Proker', 'code' => 'pr0k3r', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'News', 'code' => 'n3w5', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Surat', 'code' => '5ur4t', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Kader', 'code' => 'k4d3r', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'AUM', 'code' => '4um', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Document', 'code' => 'd0c5', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Role', 'code' => 'r0l3', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Account', 'code' => '4cc5', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'PDA', 'code' => 'pd4', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'PCA', 'code' => 'pd4', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Ranting', 'code' => 'r4nt1ng', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Majelis', 'code' => 'm47el15', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Filetype', 'code' => 'f1l3', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Bidang Usaha', 'code' => 'b1d4ng', 'tipe_menu' => 'Menu']);
-        Menu::create(['name' => 'Periode', 'code' => 'p3r10d3', 'tipe_menu' => 'Menu']);
+        Menu::create(['name' => 'Proker', 'code' => 'pr0k3r', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'News', 'code' => 'n3w5', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Surat', 'code' => '5ur4t', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Kader', 'code' => 'k4d3r', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'AUM', 'code' => '4um', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Document', 'code' => 'd0c5', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Role', 'code' => 'r0l3', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Account', 'code' => '4cc5', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'PDA', 'code' => 'pd4', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'PCA', 'code' => 'pd4', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Ranting', 'code' => 'r4nt1ng', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Majelis', 'code' => 'm47el15', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Filetype', 'code' => 'f1l3', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Bidang Usaha', 'code' => 'b1d4ng', 'tipe_menu' => 'Menu', 'created_by' => '1']);
+        Menu::create(['name' => 'Periode', 'code' => 'p3r10d3', 'tipe_menu' => 'Menu', 'created_by' => '1']);
 
     }
 
